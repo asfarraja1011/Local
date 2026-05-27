77dfb759c406ef5c511a1cbbcfce47d2f0764e74
asfarraja1011 <asfarraja14311@gmail.com>
2026-05-27 14:12:50 +0530

    Second

--- src/org/git/FirstPush.java
+++ src/org/git/FirstPush.java
@@ -4,6 +4,6 @@ public class FirstPush {
 
 	public static void main(String[] args) {
 		
-		System.out.println("The Code had Added on the Git");
+		System.out.println("Second time Pushing the code");
 	}
 }
